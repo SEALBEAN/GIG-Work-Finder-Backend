7a5ebe3a92f71381696c4babdd635ff5ee884133
SEALBEAN <xuhet2017@gmail.com>
2022-10-19 17:00:07 +0700

    "Update Get City API"

--- src/main/java/com/seal/api/gwf/controller/LocationController.java
+++ src/main/java/com/seal/api/gwf/controller/LocationController.java
@@ -4,10 +4,7 @@ import com.seal.api.gwf.dto.create.City;
 import com.seal.api.gwf.entity.LocationEntity;
 import com.seal.api.gwf.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +19,9 @@ public class LocationController {
     @GetMapping("/Province/ALL")
     public List<String> getAllProvince() {return locationService.getAllProvince();}
 
-    @GetMapping("/City/{province}")
-    public List<City> getCitysByProvince(@PathVariable String province) {return locationService.getCitysByProvince(province);}
+    @PostMapping("/City")
+    public List<City> getCitysByProvince(@RequestParam String province) {
+        System.out.println(province);
+        return locationService.getCitysByProvince(province);
+    }
 }
