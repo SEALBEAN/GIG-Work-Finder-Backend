ccf6d4640613f8a9141722293326350bec753934
SEALBEAN <xuhet2017@gmail.com>
2022-10-26 01:14:44 +0700

    Add ApplicantAccount Repo

--- src/main/java/com/seal/api/gwf/repository/ApplicantRepository.java
+++ src/main/java/com/seal/api/gwf/repository/ApplicantRepository.java
@@ -2,8 +2,12 @@ package com.seal.api.gwf.repository;
 
 import com.seal.api.gwf.entity.ApplicantEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
 
 @Repository
 public interface ApplicantRepository extends JpaRepository<ApplicantEntity, Integer> {
@@ -11,4 +15,10 @@ public interface ApplicantRepository extends JpaRepository<ApplicantEntity, Inte
 
     @Query(value = "SELECT * FROM Applicant WHERE AccountID = ?1", nativeQuery = true)
     ApplicantEntity getByAccountID(int id);
+
+    @Modifying
+    @Query(value = "INSERT INTO Applicant(LocationID, DegreeID, FirstName, LastName, Phone, DOB, Gender, Email, Address, Verify, Status, Description) " +
+            "VALUES (:locationID, :degreeID, :firstName, :lastName, :phone, :dob, :gender, :email, :address, 1, 1, :description)",nativeQuery = true)
+    @Transactional
+    Integer addApplicant(Integer locationID, Integer degreeID, String firstName, String lastName, String phone, Timestamp dob, String gender, String email, String address, String description);
 }
