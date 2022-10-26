f3b363eec068d586d875e3852c32e8dfee66efc2
SEALBEAN <xuhet2017@gmail.com>
2022-10-27 01:43:24 +0700

    Update JORepo

--- src/main/java/com/seal/api/gwf/repository/JobOfferRepository.java
+++ src/main/java/com/seal/api/gwf/repository/JobOfferRepository.java
@@ -50,7 +50,7 @@ public interface JobOfferRepository extends JpaRepository <JobOfferEntity, Integ
     @Modifying
     @Query(value = """
             UPDATE JobOffer
-            SET TypeID = ?2, LocationID = ?3, DegreeID = ?4, NumOfRecruit = ?5, OfferEndTIme = ?6, CreatedDate = ?7, Salary = ?8, Age = ?9, JobDescription = ?10, Other = ?11, StartTime = ?12, EndTime = ?13, Address = ?14, BusinessID = ?15\s
+            SET TypeID = ?2, LocationID = ?3, DegreeID = ?4, NumOfRecruit = ?5, OfferEndTIme = ?6, CreatedDate = ?7, Salary = ?8, Age = ?9, JobDescription = ?10, Other = ?11, StartTime = ?12, EndTime = ?13, Address = ?14, BusinessID = ?15
             WHERE OfferID = ?1""", nativeQuery = true)
     @Transactional
     int updateJobOffer(Integer offerID, Integer jobType, Integer location, Integer degree, Integer numOfRecruit, Object offerEndTime, Timestamp createdDate, Integer salary, Integer age, String jobDescription, String other, Time startTime, Time endTime, String address, Integer business);
